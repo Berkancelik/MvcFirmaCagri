538b14b6f9c7db38f572dc0223622e37b03b800c
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-02-23 16:09:07 +0300

    AktifCagir & YeniCagri Has been completed

--- MvcFirmaCagri/Controllers/DefaultController.cs
+++ MvcFirmaCagri/Controllers/DefaultController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcFirmaCagri.Models.Entity;
 namespace MvcFirmaCagri.Controllers
 {
     public class DefaultController : Controller
@@ -14,9 +14,34 @@ namespace MvcFirmaCagri.Controllers
             return View();
         }
 
+        DbisTakipEntities db = new DbisTakipEntities();
         public ActionResult AktifCagrilar()
         {
+            var cagrilar = db.TblCagrilars.Where(x=>x.Durum==true && x.CagriFirma==4).ToList();
+            return View(cagrilar);
+        }
+
+        public ActionResult PasifCagrilar()
+        {
+            var cagrilar = db.TblCagrilars.Where(x => x.Durum == false && x.CagriFirma == 4).ToList();
+            return View(cagrilar);
+        }
+
+        // sayfa yüklendiği zaman  ne olsun : aşağıdaki o anlama gelmektedir.
+        [HttpGet]
+        public ActionResult YeniCagri()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCagri(TblCagrilar p )
+        {
+            p.Durum = true;
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CagriFirma = 4;
+            db.TblCagrilars.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AktifCagrilar");
         }
     }
 }
