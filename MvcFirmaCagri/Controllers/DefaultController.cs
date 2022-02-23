493f01ff67bef464e2e8352ef171a38e42692671
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-02-23 15:19:49 +0300

    Aktif Cagrilar has been completed

--- MvcFirmaCagri/Controllers/DefaultController.cs
+++ MvcFirmaCagri/Controllers/DefaultController.cs
@@ -13,5 +13,10 @@ namespace MvcFirmaCagri.Controllers
         {
             return View();
         }
+
+        public ActionResult AktifCagrilar()
+        {
+            return View();
+        }
     }
 }
